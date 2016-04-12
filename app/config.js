f169fc9d1357220f430c8b3e80329eea07cdcf24
Theo Pack Test <tf.pack@gmail.com>
2016-04-12 18:41:38 +0200

    Add standalone version

--- app/config.js
+++ app/config.js
@@ -2,27 +2,33 @@ var Etcd = require('node-etcd');
 
 var etcdHost = process.env.ETCD_HOST || '172.17.8.101';
 var etcdPort = process.env.ETCD_PORT || 4001;
+var standalone = process.env.STANDALONE || false;
+var rethink;
+if (standalone) {
+  rethink = "rethinkdb";
+} else {
+  console.log('Connecting to etcd on '+etcdHost+':'+etcdPort);
+  var etcd = new Etcd(etcdHost, etcdPort);
 
-console.log('Connecting to etcd on '+etcdHost+':'+etcdPort);
-var etcd = new Etcd(etcdHost, etcdPort);
+  var rethinks = etcd.getSync("/services/rethinkdb", {recursive: true})
 
-var rethinks = etcd.getSync("/services/rethinkdb", {recursive: true})
-
-var nodes = rethinks.body.node.nodes;
-nodes.forEach(function (node){
-  console.log('Available rethinkdb server on', node.value);
-});
-var rethink = nodes[0].value;
-console.log('Connecting to Rethinkdb on', rethink)
+  var nodes = rethinks.body.node.nodes;
+  nodes.forEach(function (node){
+    console.log('Available rethinkdb server on', node.value);
+  });
+  rethink = nodes[0].value;
+  console.log('Connecting to Rethinkdb on', rethink)
+}
 
 module.exports = {
   rethinkdb: {
     host: rethink,
     port: 28015,
     authKey: '',
-    db: 'test'
+    db: 'demo'
   },
   express: {
      port: 3000
   }
 };
+
